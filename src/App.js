f3ab9311777382eedfb79e5c55d67ffafb54a146
DevLboffice <lb.devendra.lifebyte@gmail.com>
2024-06-14 11:54:31 +1000

    bootstrap layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Calculator } from "./components/Calculator";
 
 function App() {
-  return <div>prank calculator</div>;
+  return <div className="wrapper d-flex justify-content-center align-items-center" style={{
+    minHeight: "100vh"
+  }}>
+    <Calculator/>
+    </div>;
 }
 
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+export const Calculator = () => {
+  return (
+    <div className='bg-warning p-2'>
+        <div className="container text-center ">
+        <div className='row bg-dark-subtle mb-1'>display</div>
+    <div className="row gap-2 mb-1">
+      <div className="col bg-warning-subtle ">
+        AC
+      </div>
+      <div className="col bg-warning-subtle ">
+        C
+      </div>
+      <div className="col bg-warning-subtle ">
+        %
+      </div>
+      <div className="col bg-warning-subtle ">
+        /
+      </div>
+    </div>
+    <div className="row gap-2 mb-1">
+      <div className="col bg-warning-subtle ">
+        7
+      </div>
+      <div className="col bg-warning-subtle ">
+        8
+      </div>
+      <div className="col bg-warning-subtle ">
+        9
+      </div>
+      <div className="col bg-warning-subtle ">
+        *
+      </div>
+    </div>
+    <div className="row gap-2 mb-1">
+      <div className="col bg-warning-subtle ">
+        4
+      </div>
+      <div className="col bg-warning-subtle ">
+        5
+      </div>
+      <div className="col bg-warning-subtle ">
+        6
+      </div>
+      <div className="col bg-warning-subtle ">
+        -
+      </div>
+    </div>
+    <div className="row gap-2 mb-1">
+      <div className="col bg-warning-subtle ">
+        1
+      </div>
+      <div className="col bg-warning-subtle ">
+        2
+      </div>
+      <div className="col bg-warning-subtle ">
+        3
+      </div>
+      <div className="col bg-warning-subtle ">
+        +
+      </div>
+    </div>
+    <div className="row gap-2 mb-1">
+      <div className="col bg-warning-subtle ">
+        0
+      </div>
+      <div className="col bg-warning-subtle ">
+        .
+      </div>
+      <div className="col bg-warning-subtle ">
+        =
+      </div>
+    </div>
+    
+  </div>
+    </div>
+    
+  )
+}
